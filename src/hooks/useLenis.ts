cb1a983f64783251a12dbb38ab97501fc9e5fd13
prem-web.dev <a70064182@gmail.com>
2026-05-09 00:25:05 +0530

    Update useLenis.ts

--- src/hooks/useLenis.ts
+++ src/hooks/useLenis.ts
@@ -4,18 +4,24 @@ import Lenis from 'lenis';
 export function useLenis() {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
-      smoothWheel: true
+      duration: 1.4,
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      smoothTouch: false,
+      wheelMultiplier: 1,
+      touchMultiplier: 2,
     });
 
-    const raf = (time: number) => {
+    function raf(time: number) {
       lenis.raf(time);
       requestAnimationFrame(raf);
-    };
+    }
 
     requestAnimationFrame(raf);
 
-    return () => lenis.destroy();
+    return () => {
+      lenis.destroy();
+    };
   }, []);
 }
 
